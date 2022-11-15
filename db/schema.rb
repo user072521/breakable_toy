6696b5be2cf32af4f92db9a8ffed7b46439da44c
Marko Onyskiv <markoonyskiv@Markos-MacBook-Pro-2.local>
2022-11-15 16:38:35 -0500

    Index Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_13_215940) do
+ActiveRecord::Schema.define(version: 2022_11_14_185741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "resorts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "website", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Resort.create(name: 'Blue Hills Ski Area', website: 'https://www.bluehillsboston.com/', city: 'Canton', state: 'MA')
+Resort.create(name: 'Wachusett Mountain', website: 'https://www.wachusett.com/', city: 'Westminster', state: 'MA')
+Resort.create(name: 'Sugarloaf', website: 'https://www.sugarloaf.com/', city: 'Carrabassett Valley', state: 'ME')
+Resort.create(name: 'Sunday River', website: 'https://www.sundayriver.com/', city: 'Newry', state: 'ME')
